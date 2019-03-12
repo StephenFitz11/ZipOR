93dd836f77e73e9ed17120bc785dd296d25c41c0
StephenFitz11"git config --global user.email smf0211@gmail.comStephenFitz11git config --global user.name 'StephenFitz11 <smf0211@gmail.com>
2019-03-11 20:31:31 -0500

    worked on backend math to calculate weighted NRIs

--- WI_calculator_master.py
+++ WI_calculator_master.py
@@ -1,17 +1,4 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pprint
-
-
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
-
-sheet = client.open('WI-Input').sheet1
-
-pp = pprint.PrettyPrinter()
-owners = sheet.get_all_records()
-
+import pprint as pp
 
 def get_royalty_acres(list_data):
     x = [i['Acres'] * i['Royalty'] for i in list_data]
@@ -21,14 +8,60 @@ def get_royalty_acres(list_data):
 def get_total_acres(list_data):
     total_acres = 0
     for i in list_data:
-        total_acres = total_acres + i['Acres']
+        total_acres = total_acres + i['acres']
     return total_acres
 
 
+input_list = [{'name': 'CHK', 'acres': 80, 'NRI': 0.75}, {'name': 'CHK', 'acres': 80, 'NRI': 0.875}, {'name': 'CHK', 'acres': 160, 'NRI': 0.75},
+              {'name': 'Roan', 'acres': 80, 'NRI': 0.8125}, {'name': 'Roan', 'acres': 80, 'NRI': 0.625}, {'name': 'Buffalo', 'acres': 80, 'NRI': 0.75},
+              {'name': 'Buffalo', 'acres': 40, 'NRI': 0.785}, {'name': 'Buffalo', 'acres': 40, 'NRI': 0.8369}]
 
 
-weighted_nri = get_royalty_acres(owners) / get_total_acres(owners)
 
-row = owners[0]['Name'], get_total_acres(owners), weighted_nri
-index = 10
-sheet.insert_row(row, index)
+
+for row in input_list:
+    roy_acre = row['acres'] * row['NRI']
+    row['royalty acres'] = roy_acre
+
+
+
+index_value = -1
+same_entity = []
+sorted_input = sorted(input_list, key = lambda i: i['name'],reverse=True)
+
+
+
+
+
+for i in sorted_input:
+    index_value += 1
+    name_sort = sorted_input[index_value]
+
+
+    print(name_sort)
+    # try:
+    #     if name_sort == sorted_input[index_value + 1]['name']:
+    #         same_entity.append(i)
+    # except IndexError:
+    #     pass
+
+
+
+
+
+
+
+
+
+
+
+#     Create the backend program that does the math.
+#     main_list = [{'Name': v,'acres': v,'NRI': v}, {'Name': v,'acres': v,'NRI': v}, {'Name': v,'acres': v,'NRI': v,}]
+#     royalty_acres = [''
+#     Each row will be stored as a dictionary with the column headers as the keys.
+#     Take each row and append it to the main_list
+#     for i(row) in main_list
+#         roy_acre = 'NRI' key * 'acre key'
+#         royalty_acres.append(i)
+#         royalty_acres['roy_acres'] = roy_acre
+
---
+++ data_testing.py
@@ -0,0 +1,7 @@
+input_list = [{'name': 'CHK', 'acres': 80, 'NRI': 0.75}, {'name': 'CHK', 'acres': 80, 'NRI': 0.875}, {'name': 'CHK', 'acres': 160, 'NRI': 0.75},
+              {'name': 'Roan', 'acres': 80, 'NRI': 0.8125}, {'name': 'Roan', 'acres': 80, 'NRI': 0.625}, {'name': 'Buffalo', 'acres': 80, 'NRI': 0.75},
+              {'name': 'Buffalo', 'acres': 40, 'NRI': 0.785}, {'name': 'Buffalo', 'acres': 40, 'NRI': 0.8369}]
+
+
+example = set()
+dir(example)
