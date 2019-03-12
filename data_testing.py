a6758820ce543b75269413b4ca7c9c8b5fab2f6e
StephenFitz11"git config --global user.email smf0211@gmail.comStephenFitz11git config --global user.name 'StephenFitz11 <smf0211@gmail.com>
2019-03-12 07:44:36 -0500

    playing with json

--- data_testing.py
+++ data_testing.py
@@ -1,9 +1,3 @@
-input_list = [{'name': 'CHK', 'acres': 80, 'NRI': 0.75}, {'name': 'CHK', 'acres': 80, 'NRI': 0.875}, {'name': 'CHK', 'acres': 160, 'NRI': 0.75},
-              {'name': 'Roan', 'acres': 80, 'NRI': 0.8125}, {'name': 'Roan', 'acres': 80, 'NRI': 0.625}, {'name': 'Buffalo', 'acres': 80, 'NRI': 0.75},
-              {'name': 'Buffalo', 'acres': 40, 'NRI': 0.785}, {'name': 'Buffalo', 'acres': 40, 'NRI': 0.8369}]
+import json
 
-print(input_list[3]['name'])
-
-
-for i in input_list:
-    print(i)
+json.load()
